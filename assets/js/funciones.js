dec39a0c578b68e6194d04a57330d5660030042a
[Sergio AM] <[sergio.alvarez.monroy@gmail.com]>
2021-10-26 16:31:25 +0200

    Añadida ficha de lectura y la habilitación de la edicion de la ficha

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
     $("select.especialidad").change(function(){
         var optionValue = $(this).val();
+        $(".bloque-inicial").hide();
         $(".bloque-especialidad").hide();
         $(".bloque-especialidad." + optionValue).show();
         $(".bloque-especialidad." + optionValue).css("display", "block");
         $('.programa').prop('selectedIndex',0);
         $('.experiencia').prop('selectedIndex',0);
     });
+    $("#editar-perfil").click(function(){
+        $("select").prop('disabled', false);
+        $("input").prop('disabled', false);
+        $("textarea").prop('disabled', false);
+    })
 });
