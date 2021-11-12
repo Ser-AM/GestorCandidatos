26104c704f10313a421b1a7740d36a19ef7eabdd
cristianurba <cristianurba@gmail.com>
2021-11-12 13:48:31 +0100

    Buscar por software FUNCIONA

--- assets/php/buscarSoftwareCandidatos.php
+++ assets/php/buscarSoftwareCandidatos.php
@@ -22,7 +22,7 @@
         </header>
         <?php
             // No muestra los errores como posibles "undefined" de campos que no han sido rellenados
-            //error_reporting(E_ERROR | E_PARSE);
+            error_reporting(E_ERROR | E_PARSE);
 
             // Variables que recibimos del formulario de insertarCandidato
             
@@ -56,27 +56,59 @@
             // Se especifica y ejecuta la query
             
             $queryDatosCandidato = "SELECT * FROM `datos` JOIN `softwares` ON 
-                datos.TELEFONO = softwares.TELEFONO WHERE
-                /*ESPECIALIDAD LIKE '%$especialidad%' AND*/
-                TITULO LIKE '%$titulo%'/* AND
-                SECTOR LIKE '%$sector%' AND
-                NOTAS LIKE '%$notas%' AND
-                SOFTWARE LIKE '$software1'*/";
-
-            //$querySoftwareCandidato = "SELECT * FROM `softwares` WHERE
-              //  SOFTWARE = '$software1'";
+                    datos.TELEFONO = softwares.TELEFONO WHERE
+                    ESPECIALIDAD LIKE '%$especialidad%' AND
+                    TITULO LIKE '%$titulo%' AND
+                    SECTOR LIKE '%$sector%' AND
+                    SOFTWARE LIKE '%$software1%' AND
+                    NOTAS LIKE '%$notas%'";
 
             $resultadosDatos = mysqli_query($conexion, $queryDatosCandidato);
-            
+            $nombreCandidato = " ";
             if($resultadosDatos == false){
                 echo "Error al encontrar los candidatos. " . mysqli_error($conexion);
             } else {
                 while(($fila = mysqli_fetch_array($resultadosDatos, MYSQLI_ASSOC))){
-                    echo $fila['NOMBRE'] . " " . $fila['APELLIDOS'] . " " . $fila['EMAIL'] . " " . $fila['TITULO'] . " " . $fila['SOFTWARE'];
-                };
+                    $telefonoCandidato = $fila['TELEFONO'];
+                    if($nombreCandidato != $fila['NOMBRE']){
+                    $nombreCandidato = $fila['NOMBRE'];
+
+                echo "<div class='resultadoCandidato'>";
+                echo    "<div class='contenedorResultado'>";
+                echo        "<h1>". $fila['NOMBRE'] . " " . $fila['APELLIDOS']."</h1>";
+                echo        "<p>".$fila['TELEFONO']."</p>";
+                echo        "<h3>Especialidad: ".$fila['ESPECIALIDAD']."</h3>";
+                echo        "<div class='softwareExperiencia'>";
+                
+
+                $queryBuscarSoftwareCandidato = "SELECT * FROM `softwares` WHERE TELEFONO = '$telefonoCandidato'";
+                $resultadosSoftware = mysqli_query($conexion, $queryBuscarSoftwareCandidato);
+
+                // NO FUNCIONA BIEN ASÍ. SE INSERTAN EN EL PRIMER CANDIDATO QUE APAREZCA TODOS LOS SOFTWARES QUE ENCUENTRA
+                if($resultadosSoftware == false){
+                    echo "No se han encontrado softwares para este candidato";
+                }else{
+                    while(($fila2 = mysqli_fetch_array($resultadosSoftware, MYSQLI_ASSOC))){
+                    echo   "<p>".$fila2['SOFTWARE']."  ".$fila2['EXPERIENCIA']."</p>";
+                    }
+                }
+
+                echo        "</div>";
+                echo    "</div>";
+                echo    "<div class='contenedorResultado'>";
+                echo        "<div class='botones-candidato'>";
+                echo        "<button id='boton-ver-cv'><a href='../../cvs/CV".$fila['TELEFONO']."' target='_blank'>Ver CV</a></button>";
+                echo        "<button id='boton-ver-perfil'><a href='perfilCandidato.php?tln=$telefonoCandidato' target='_self'>Ver perfil</button></a>"; 
+                echo        "</div>";
+                echo        "<div class='notas-candidato-resultado'>";
+                echo            "<h5>Notas: </h5>";
+                echo            "<p>  ". $fila['NOTAS'] ."</p>";
+                echo        "</div>";
+                echo    "</div>";
+                echo "</div>";
+                
+                }};
             }
-
-
 
             // Cerramos la conexión
             mysqli_close($conexion);
